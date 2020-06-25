85904dc65bfe4d4bc54d2200b5a55c33764ceae1
awojciechowski_BlueSoft <awojciechowski_BlueSoft@bitbucket.org>
2020-06-25 14:28:19 +0200

    zmiany w logice identyfikowania grafów

--- src/main/java/com/company/Manager.java
+++ src/main/java/com/company/Manager.java
@@ -8,23 +8,33 @@ import java.util.stream.Stream;
 public class Manager {
     public static void processData(String inputPath) {
 
+        final Integer[] graphs_status = new Integer[2];
+        graphs_status[0] = 0;   //CONTAINS NUMBER OF GRAPHS IDENTIFIED;
+        graphs_status[1] = 0;   //CONTAIN MAXIMUM NUMBER OF OCCURED VERTICES;
+
         try {
             Stream<String> stream = Files.lines(Paths.get(inputPath));
-
-        final Integer[] counter = {0};
-        final String[] temp = {""};
 
             stream
                 .skip(1)
                 .forEach(s -> {
-                    if (!( s.split(" ")[0].equals(temp[0])))
+                    //declare current variables
+                    Integer current_1st_value = Integer.parseInt(s.split(" ")[0]);
+                    Integer current_2nd_value = Integer.parseInt(s.split(" ")[1]);
+                    //Process
+                    // IF (1ST VERTEX > MAX) THEN NEW GRAPH FOUNDED
+                    if(current_1st_value > graphs_status[1])
                     {
-                        counter[0]++;
-                        temp[0] = s.split(" ")[1];
+                        graphs_status[0] += 1; // GRAPH COUNT ++
+
+                        //graphs_status[1] = current_2nd_value;
                     }
+                    // NEW MAX IS EACH TIME TAKEN FROM 2ND VERTEX (2ND IS ALWAYS GREATER THEN 1ST ) ASSUMPTION
+                    graphs_status[1] = current_2nd_value;
                 });
 
-            System.out.println("counter : "+ counter[0]);
+        System.out.println(graphs_status[0]);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
